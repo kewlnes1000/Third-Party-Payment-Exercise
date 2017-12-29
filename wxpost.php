08b486df14cfde4e0e0a52b68fd79cc113afe61f
Tai-Siang <kewlnes2000@gmail.com>
2017-12-29 10:04:51 +0800

    Update wxpost.php

--- wxpost.php
+++ wxpost.php
@@ -94,7 +94,7 @@ $array = json_decode(json_encode($xml), 1);
      <meta http-equiv="content-Type" content="text/html; charset=utf-8" />
    </head>
    <body>
-     <form action="./qrcode.php"; method="get" id="frm1" target="_blank" target="_blank">
+     <form action="./qrcode.php"; method="get" id="frm1" target="_blank" >
          <p>正在为您跳转中，请稍候......</p>
          <input type="hidden" name="code" id="code_url" value="<?php echo $array["code_url"]; ?>"/>
 
